6a6e4b89d15f64be144dbd197272b6167f74a2b8
Cassondra Roberts <castastrophe@users.noreply.github.com>
2026-07-01 12:33:57 -0400

    chore: relicense to MPL-2.0 & migrate domain to allons-y.studio

--- index.js
+++ index.js
@@ -1,13 +1,9 @@
 /*!
-Copyright 2023. All rights reserved.
-This file is licensed to you under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License. You may obtain a copy
-of the License at http://www.apache.org/licenses/LICENSE-2.0
+Copyright 2023 Cassondra Roberts
 
-Unless required by applicable law or agreed to in writing, software distributed under
-the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR REPRESENTATIONS
-OF ANY KIND, either express or implied. See the License for the specific language
-governing permissions and limitations under the License.
+This Source Code Form is subject to the terms of the Mozilla Public
+License, v. 2.0. If a copy of the MPL was not distributed with this
+file, You can obtain one at https://mozilla.org/MPL/2.0/.
 */
 
 import { promises as fsp } from "fs";
@@ -74,7 +70,7 @@ export default async function (
 		try {
 			base64 = Buffer.from(sourcemap)?.toString("base64");
 		}
-		catch (e) {
+		catch {
 			/* empty */
 		}
 
